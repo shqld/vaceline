29aa513d4547d0b60c62191800460f96d7fc49f3
Sho Miyamoto <shqld8@gmail.com>
2019-07-28 20:34:14 +0900

    update: Location interface

--- src/parser/create-error.ts
+++ src/parser/create-error.ts
@@ -1,53 +1,25 @@
 import chalk from 'chalk'
 import { assert } from '../utils/assert'
+import { Position } from '../nodes'
 
 const MARGIN = 2
 const HORIZONTAL_MARK = '> '
 const VERTICAL_MARK = '^'
 
-const getLocation = (lines: Array<string>, start: number, end?: number) => {
-  if (end && start > end)
-    throw new Error('invalid end offset (end number is less than start)')
-
-  let acc = 0
-  let line: number
-  let column: number
-  let range: number = 1
-
-  for (let i = 0; i < lines.length; i++) {
-    const lineLength = lines[i].length
-    acc += lineLength
-
-    if (acc > start) {
-      // Add 1 because line & col count from 1
-      line = i + 1
-      column = start - (acc - lineLength) + 1
-
-      if (end) {
-        if (end + 1 > acc)
-          throw new Error('invalid end offset (end number exceeds line)')
-
-        range = end - start + 1
-      }
-      break
-    }
-  }
-
-  if (line! === undefined || column! === undefined)
-    throw new Error('invalid start offset')
-
-  return { line, column, range }
-}
-
 export const createError = (
   source: string,
   message: string,
-  start: number,
-  end?: number
+  start: Position,
+  end?: Position
 ): SyntaxError => {
   const lines = source.split('\n')
 
-  const loc = getLocation(lines, start, end)
+  // const loc = getLocation(lines, start, end)
+  const loc = {
+    line: start.line,
+    column: start.column,
+    range: end ? end.column - start.column : 1,
+  }
 
   const topLine = loc.line - MARGIN - 1 > 0 ? loc.line - MARGIN - 1 : 0
   const bottomLine =
