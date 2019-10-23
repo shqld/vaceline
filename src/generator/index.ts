ca04daca18ffafab7f3438100ff2d5f17fc1fce2
Sho Miyamoto <me@shomiyamoto.dev>
2019-10-23 11:36:48 +0900

    add: ast traversal for building doc to print

--- src/generator/index.ts
+++ src/generator/index.ts
@@ -1,7 +1,7 @@
-import { Node } from '../nodes'
-import { doc, Doc, util } from 'prettier'
+import { Node, BaseNode } from '../nodes'
+import { doc as docHelpers, Doc, util } from 'prettier'
 
-export const { builders } = doc
+export const { builders } = docHelpers
 
 const defaultPrintOptions = {
   printWidth: 100,
@@ -9,9 +9,64 @@ const defaultPrintOptions = {
   useTabs: false,
 }
 
+const traverseArrayForNode = <
+  D extends Doc,
+  K extends keyof D,
+  T extends D & { [k in K]: Array<Doc | Node> }
+>(
+  doc: D,
+  prop: K
+): D => {
+  const arr: Array<Doc | Node> = (doc as T)[prop]
+
+  let i = arr.length
+
+  while (i--) {
+    const item = arr[i]
+
+    if (item instanceof BaseNode) {
+      arr[i] = generateDocFromAst(item)
+    }
+  }
+
+  return doc
+}
+
+const generateDocFromAst = (ast: Node): Doc => {
+  const doc = ast.print()
+
+  if (typeof doc === 'string') {
+    return doc
+  }
+
+  switch (doc.type) {
+    case 'align':
+      return traverseArrayForNode(doc, 'contents')
+    case 'concat':
+    case 'fill':
+      return traverseArrayForNode(doc, 'parts')
+    case 'group':
+      return traverseArrayForNode(doc, 'contents')
+    case 'if-break':
+      return traverseArrayForNode(
+        traverseArrayForNode(doc, 'breakContents'),
+        'flatContents'
+      )
+    case 'indent':
+      return traverseArrayForNode(doc, 'contents')
+    case 'line':
+    case 'line-suffix':
+    case 'line-suffix-boundary':
+    default:
+      return doc
+  }
+}
+
 export const generate = (ast: Node): { code: string; map?: string } => {
-  const { formatted } = doc.printer.printDocToString(
-    ast.print(),
+  const doc = generateDocFromAst(ast)
+
+  const { formatted } = docHelpers.printer.printDocToString(
+    doc,
     defaultPrintOptions
   )
 
