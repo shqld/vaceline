4a5becfb1d81d2e4054dc2f6f5977793054e35ca
Sho Miyamoto <me@shomiyamoto.dev>
2019-11-20 00:16:08 +0900

    update: format/generation snapshot testing

--- __tests__/code-generation.ts
+++ __tests__/code-generation.ts
@@ -1,16 +1,43 @@
-import { wrap } from 'jest-snapshot-serializer-raw'
+import { wrap as wrapRaw } from 'jest-snapshot-serializer-raw'
 import { testdocs } from './helpers/snapshot'
 import { generate } from '../src'
+import { Tokenizer } from '../src/parser/tokenizer'
 
 for (const testdoc of testdocs) {
   for (const testcase of testdoc.basic) {
     describe(`(${testdoc.name})`, () => {
-      it(testcase.name, () => {
-        const ast = testdoc.parse.call(testdoc.parse, testcase.code)
-        const { code } = generate(ast)
+      const ast = testdoc.parse.call(testdoc.parse, testcase.code)
+      const { code } = generate(ast)
 
-        expect(wrap(code)).toMatchSnapshot()
+      it(testcase.name, () => {
+        const output = new Tokenizer(code)
+          .tokenize()
+          .filter((token) => token.type !== 'comment')
+          .map((token) => token.value)
+
+        const input = new Tokenizer(testcase.code)
+          .tokenize()
+          .filter((token) => token.type !== 'comment')
+          .map((token) => token.value)
+
+        expect(output).toStrictEqual(input)
+      })
+
+      it('format', () => {
+        expect(wrap(testcase.code, code)).toMatchSnapshot()
       })
     })
   }
 }
+
+const wrap = (input: string, output: string) =>
+  wrapRaw(
+    `
+---------- (input) ---------------------------------------------------------------
+${input}
+
+---------- (output) --------------------------------------------------------------
+${output}
+
+`.trim()
+  )
