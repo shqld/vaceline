1bf9e3363ec78c44e9500b38fe3cc4458666859d
Sho Miyamoto <me@shomiyamoto.dev>
2019-12-13 13:00:35 +0900

    fix(parser): type errors in `parseIdentifier`

--- src/parser/expression/identifier.ts
+++ src/parser/expression/identifier.ts
@@ -10,53 +10,64 @@ export const parseIdentifier = (
     n.Identifier,
     p.startNode(),
     {
-      name: token!.value,
+      name: (token ?? p.read()).value,
     }
   )
 ): n.NodeWithLoc<n.Member | n.ValuePair | n.Identifier> => {
-  // Member
   if (isToken(p.peek(), 'symbol', '.')) {
     p.take()
-
-    const memberTok = p.read()
-    const member = p.finishNode(n.Identifier, p.startNode(), {
-      name: memberTok.value,
-    })
-
-    const expr = new n.Member({
-      base,
-      member,
-    })
-
-    expr.loc = {
-      start: base.loc.start,
-      end: member.loc.end,
-    }
-
-    return parseIdentifier(p, undefined, expr as n.NodeWithLoc<n.Member>)
+    return parseIdentifier(p, undefined, parseMember(p, base))
   }
 
-  // ValuePair
   if (isToken(p.peek(), 'symbol', ':')) {
     p.take()
-
-    const nameTok = p.read()
-    const name = p.finishNode(n.Identifier, p.startNode(), {
-      name: nameTok.value,
-    })
-
-    const expr = new n.ValuePair({
-      base,
-      name,
-    })
-
-    expr.loc = {
-      start: base.loc.start,
-      end: name.loc.end,
-    }
-
-    return expr as n.NodeWithLoc<n.ValuePair>
+    // return the expression here because there won't be any recursive pattern
+    return parseValuePair(p, base)
   }
 
   return base
+}
+
+export const parseMember = (
+  p: Parser,
+  base: n.NodeWithLoc<n.Identifier | n.Member>
+): n.NodeWithLoc<n.Member> => {
+  const memberTok = p.read()
+  const member = p.finishNode(n.Identifier, p.startNode(), {
+    name: memberTok.value,
+  })
+
+  const expr = new n.Member({
+    base,
+    member,
+  })
+
+  expr.loc = {
+    start: base.loc.start,
+    end: member.loc.end,
+  }
+
+  return expr as n.NodeWithLoc<n.Member>
+}
+
+export const parseValuePair = (
+  p: Parser,
+  base: n.NodeWithLoc<n.Identifier | n.Member>
+): n.NodeWithLoc<n.ValuePair> => {
+  const nameTok = p.read()
+  const name = p.finishNode(n.Identifier, p.startNode(), {
+    name: nameTok.value,
+  })
+
+  const expr = new n.ValuePair({
+    base,
+    name,
+  })
+
+  expr.loc = {
+    start: base.loc.start,
+    end: name.loc.end,
+  }
+
+  return expr as n.NodeWithLoc<n.ValuePair>
 }
