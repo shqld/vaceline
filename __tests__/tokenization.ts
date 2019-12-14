0f186165af7c5882bb05cf040c8bb9024829c8a7
Sho Miyamoto <me@shomiyamoto.dev>
2019-12-14 18:38:15 +0900

    test: add snapshot for tokenization

---
+++ __tests__/tokenization.ts
@@ -0,0 +1,20 @@
+import { testdocs } from './helpers/snapshot'
+import { Tokenizer, Token } from '../src/parser/tokenizer'
+
+for (const testdoc of testdocs) {
+  for (const testcase of testdoc.basic) {
+    const tokens: Array<Token> = new Tokenizer(testcase.code).tokenize()
+
+    describe(`(${testdoc.name})`, () => {
+      it(testcase.name, () => {
+        expect(tokens.map(({ loc, ...token }) => token)).toMatchSnapshot()
+      })
+
+      it('<loc>' + testcase.name, () => {
+        expect(
+          tokens.map((token) => ({ type: token.type, loc: token.loc }))
+        ).toMatchSnapshot()
+      })
+    })
+  }
+}
