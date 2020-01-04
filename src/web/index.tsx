29ee001f3611e287d3b87d742412133a63d7e56a
Sho Miyamoto <me@shomiyamoto.dev>
2020-01-04 18:08:13 +0900

    improve(web): add preload

--- src/web/index.tsx
+++ src/web/index.tsx
@@ -3,14 +3,37 @@
 /* @jsx h */
 
 import { h, hydrate } from 'preact'
-import { useState } from 'preact/hooks'
+import { useState, useEffect } from 'preact/hooks'
 
 import { SourcePanel } from './source'
 import { ResultPanel } from './result'
 import { Result } from './type'
 
+declare global {
+  function requestIdleCallback(): void
+}
+
+const preloadHrefs = ['./dist/1.worker.js', './dist/2.worker.js']
+
+const raf =
+  typeof requestIdleCallback !== 'undefined'
+    ? requestIdleCallback
+    : setImmediate
+
 const App = () => {
   const [result, setResult] = useState<Result | null>(null)
+
+  useEffect(() => {
+    raf(() => {
+      preloadHrefs.forEach((href) => {
+        const link = document.createElement('link')
+        link.rel = 'preload'
+        link.href = href
+        link.as = 'script'
+        document.head.appendChild(link)
+      })
+    })
+  }, [])
 
   return (
     <main id="main" class="section">
