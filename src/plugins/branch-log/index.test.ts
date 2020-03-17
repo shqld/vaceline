00c81e170c0ae744a7c4f344b6b29db91f2dee39
Sho Miyamoto <me@shomiyamoto.dev>
2020-03-17 21:31:33 +0900

    fix: plugins/branch-log

---
+++ src/plugins/branch-log/index.test.ts
@@ -0,0 +1,78 @@
+import { parse, traverse, generate } from '../../lib'
+import BranchLogPlugin from '.'
+import { NodePath } from '../../traverser/path'
+import { d } from '../../nodes'
+
+const code = `
+
+sub vcl_recv {
+  if (true) {
+    set req.http.Test = 1;
+  } else if (false) {
+    set req.http.Test = 1;
+  } else {
+    set req.http.Test = 1;
+  }
+}
+
+sub my_function {
+  set req.http.Test = 1;
+}
+
+sub vcl_deliver {
+  call my_function;
+
+  set req.http.Test = 1;
+}
+
+`.trim()
+
+describe('BranchLogPlugin', () => {
+  const ast = parse(code)
+
+  it('should create Branch-Log nodes inside each branch', () => {
+    BranchLogPlugin(ast)
+
+    traverse(ast, {
+      entry({ node }: NodePath<d.SubroutineStatement | d.IfStatement>) {
+        if (node.is('SubroutineStatement')) {
+          if (node.id.name === 'vcl_recv') {
+            const loggerNode = node.body[0]
+
+            expect(generate(loggerNode).code).toMatch(
+              /set req\.http\.Vaceline-Branch-Log = "\(vcl_recv\).*";/
+            )
+          } else {
+            const loggerNode = node.body[0]
+
+            expect(loggerNode).toMatchObject({
+              type: 'AddStatement',
+              left: { member: { name: 'Vaceline-Branch-Log' } },
+            } as d.AddStatement)
+
+            expect(generate(loggerNode).code).toMatch(
+              /add req\.http\.Vaceline-Branch-Log = "\(.*\).*";/
+            )
+          }
+
+          if (node.id.name === 'vcl_deliver') {
+            const [stdCollect, reqToResp] = node.body.slice(-2)
+
+            expect(generate(stdCollect).code).toMatch(
+              'std.collect(req.http.Vaceline-Branch-Log);'
+            )
+            expect(generate(reqToResp).code).toMatch(
+              'set resp.http.Vaceline-Branch-Log = req.http.Vaceline-Branch-Log;'
+            )
+          }
+        } else if (node.is('IfStatement')) {
+          const loggerNode = node.consequent[0]
+
+          expect(generate(loggerNode).code).toMatch(
+            /add req\.http\.Vaceline-Branch-Log = "\(anonymous\).*";/
+          )
+        }
+      },
+    })
+  })
+})
--- src/plugins/branch-log/index.ts
+++ src/plugins/branch-log/index.ts
@@ -6,55 +6,89 @@ import {
   buildSetStatement,
   buildStringLiteral,
   buildAddStatement,
+  buildFunCallExpression,
+  buildExpressionStatement,
 } from '../../nodes/builders.gen'
+import { Node } from '../../nodes/defs'
 
-const variable = (obj: 'req' | 'resp') =>
-  buildMember(
-    buildMember(buildIdentifier(obj), buildIdentifier('http')),
-    buildIdentifier('Branch-Log')
-  )
+// req.http.Vaceline-Branch-Log
+const varBranchLog = buildMember(
+  buildMember(buildIdentifier('req'), buildIdentifier('http')),
+  buildIdentifier('Vaceline-Branch-Log')
+)
 
-// const collectLogs = new FunCallExpression({
-//   callee: new Member({
-//     base: new Identifier({
-//       name: 'std',
-//     }),
-//     member: new Identifier({
-//       name: 'collect',
-//     }),
-//   }),
-// })
+const getLoc = (node: Node) =>
+  node.loc ? `${node.loc.start.line}:${node.loc.start.column}` : 'synthethic'
 
 export default (ast: d.Node) => {
   traverse(ast, {
     entry({ node }) {
       if (node.is('SubroutineStatement')) {
-        const loggerNode =
-          node.id.name === 'vcl_deliver'
-            ? buildSetStatement(
-                variable('resp'),
-                buildStringLiteral('std.collect(std)'),
-                '='
-              )
-            : buildAddStatement(
-                variable('req'),
-                buildStringLiteral(node.id.name),
-                '='
-              )
+        if (node.id.name === 'vcl_recv') {
+          node.body.unshift(
+            /**
+             * set req.http.Vaceline-Branch-Log = "(vcl_recv)${line}:${col}";
+             */
+            buildSetStatement(
+              varBranchLog,
+              buildStringLiteral(`"(${node.id.name})${getLoc(node)}"`),
+              '='
+            )
+          )
+        } else {
+          node.body.unshift(
+            /**
+             * add req.http.Vaceline-Branch-Log = "(${nodeName})${line}:${col}";
+             */
+            buildAddStatement(
+              varBranchLog,
+              buildStringLiteral(`"(${node.id.name})${getLoc(node)}"`),
+              '='
+            )
+          )
+        }
 
-        node.body.unshift(loggerNode)
+        if (node.id.name === 'vcl_deliver') {
+          node.body.push(
+            /**
+             * set resp.http.Vaceline-Branch-Log = std.collect(
+             *   req.http.Vaceline-Branch-Log
+             * );
+             */
+            buildExpressionStatement(
+              buildFunCallExpression(
+                buildMember(buildIdentifier('std'), buildIdentifier('collect')),
+                [varBranchLog]
+              )
+            ),
+            buildSetStatement(
+              buildMember(
+                buildMember(buildIdentifier('resp'), buildIdentifier('http')),
+                buildIdentifier('Vaceline-Branch-Log')
+              ),
+              buildMember(
+                buildMember(buildIdentifier('req'), buildIdentifier('http')),
+                buildIdentifier('Vaceline-Branch-Log')
+              ),
+              '='
+            )
+          )
+        }
       } else if (node.is('IfStatement')) {
-        const loggerNode = buildAddStatement(
-          variable('req'),
-          buildStringLiteral(
-            node.loc
-              ? `${node.loc.start.line}:${node.loc.start.column}`
-              : 'synthethic'
-          ),
-          '='
+        node.consequent.unshift(
+          /**
+           * add req.http.Vaceline-Branch-Log = "${line}:${col}";
+           */
+          buildAddStatement(
+            varBranchLog,
+            buildStringLiteral(
+              node.loc
+                ? `"(anonymous)${node.loc.start.line}:${node.loc.start.column}"`
+                : '"synthethic"'
+            ),
+            '='
+          )
         )
-
-        node.consequent.unshift(loggerNode)
       }
     },
   })
