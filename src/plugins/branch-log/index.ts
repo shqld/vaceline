3a47d078ce78335bb8032a66016b21be528d6601
Sho Miyamoto <me@shomiyamoto.dev>
2021-01-29 00:54:49 +0900

    Fix plugins' definition

--- src/plugins/branch-log/index.ts
+++ src/plugins/branch-log/index.ts
@@ -1,50 +1,53 @@
 import { traverse } from '../../lib'
-import { d } from '../../nodes'
-import {
-  buildMember,
-  buildIdentifier,
-  buildSetStatement,
-  buildStringLiteral,
-  buildAddStatement,
-  buildFunCallExpression,
-  buildExpressionStatement,
-} from '../../nodes/builders.gen'
-import { Node } from '../../nodes/defs'
+import { Node } from '../../nodes'
 
 // req.http.Vaceline-Branch-Log
-const varBranchLog = buildMember(
-  buildMember(buildIdentifier('req'), buildIdentifier('http')),
-  buildIdentifier('Vaceline-Branch-Log')
-)
+const varBranchLog: Node = {
+  type: 'Member',
+  base: {
+    type: 'Member',
+    base: { type: 'Identifier', name: 'req' },
+    member: { type: 'Identifier', name: 'http' },
+  },
+  member: { type: 'Identifier', name: 'Vaceline-Branch-Log' },
+}
 
 const getLoc = (node: Node) =>
   node.loc ? `${node.loc.start.line}:${node.loc.start.column}` : 'synthethic'
 
-export default (ast: d.Node) => {
+export default (ast: Node) => {
   traverse(ast, {
     entry({ node }) {
-      if (node.is('SubroutineStatement')) {
+      if (node.type === 'SubroutineStatement') {
         if (node.id.name === 'vcl_recv') {
           node.body.unshift(
             /**
              * set req.http.Vaceline-Branch-Log = "(vcl_recv)${line}:${col}";
              */
-            buildSetStatement(
-              varBranchLog,
-              buildStringLiteral(`"(${node.id.name})${getLoc(node)}"`),
-              '='
-            )
+            {
+              type: 'SetStatement',
+              left: varBranchLog,
+              right: {
+                type: 'StringLiteral',
+                value: `"(${node.id.name})${getLoc(node)}"`,
+              },
+              operator: '=',
+            }
           )
         } else {
           node.body.unshift(
             /**
              * add req.http.Vaceline-Branch-Log = "(${nodeName})${line}:${col}";
              */
-            buildAddStatement(
-              varBranchLog,
-              buildStringLiteral(`"(${node.id.name})${getLoc(node)}"`),
-              '='
-            )
+            {
+              type: 'AddStatement',
+              left: varBranchLog,
+              right: {
+                type: 'StringLiteral',
+                value: `"(${node.id.name})${getLoc(node)}"`,
+              },
+              operator: '=',
+            }
           )
         }
 
@@ -55,39 +58,61 @@ export default (ast: d.Node) => {
              *   req.http.Vaceline-Branch-Log
              * );
              */
-            buildExpressionStatement(
-              buildFunCallExpression(
-                buildMember(buildIdentifier('std'), buildIdentifier('collect')),
-                [varBranchLog]
-              )
-            ),
-            buildSetStatement(
-              buildMember(
-                buildMember(buildIdentifier('resp'), buildIdentifier('http')),
-                buildIdentifier('Vaceline-Branch-Log')
-              ),
-              buildMember(
-                buildMember(buildIdentifier('req'), buildIdentifier('http')),
-                buildIdentifier('Vaceline-Branch-Log')
-              ),
-              '='
-            )
+            {
+              type: 'ExpressionStatement',
+              body: {
+                type: 'FunCallExpression',
+                callee: {
+                  type: 'Member',
+                  base: {
+                    type: 'Identifier',
+                    name: 'std',
+                  },
+                  member: { type: 'Identifier', name: 'collect' },
+                },
+                args: [varBranchLog],
+              },
+            },
+            {
+              type: 'SetStatement',
+              left: {
+                type: 'Member',
+                base: {
+                  type: 'Member',
+                  base: { type: 'Identifier', name: 'resp' },
+                  member: { type: 'Identifier', name: 'http' },
+                },
+                member: { type: 'Identifier', name: 'Vaceline-Branch-Log' },
+              },
+              right: {
+                type: 'Member',
+                base: {
+                  type: 'Member',
+                  base: { type: 'Identifier', name: 'req' },
+                  member: { type: 'Identifier', name: 'http' },
+                },
+                member: { type: 'Identifier', name: 'Vaceline-Branch-Log' },
+              },
+              operator: '=',
+            }
           )
         }
-      } else if (node.is('IfStatement')) {
+      } else if (node.type === 'IfStatement') {
         node.consequent.unshift(
           /**
            * add req.http.Vaceline-Branch-Log = "${line}:${col}";
            */
-          buildAddStatement(
-            varBranchLog,
-            buildStringLiteral(
-              node.loc
+          {
+            type: 'AddStatement',
+            left: varBranchLog,
+            right: {
+              type: 'StringLiteral',
+              value: node.loc
                 ? `"(anonymous)${node.loc.start.line}:${node.loc.start.column}"`
-                : '"synthethic"'
-            ),
-            '='
-          )
+                : '"synthethic"',
+            },
+            operator: '=',
+          }
         )
       }
     },
