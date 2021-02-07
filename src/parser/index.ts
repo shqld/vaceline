79c8d4c9c158fa833e35a080c1bafd81917bfebb
Sho Miyamoto <me@shomiyamoto.dev>
2021-02-07 12:25:31 +0900

    feat: add Parser.parseNode

--- src/parser/index.ts
+++ src/parser/index.ts
@@ -1,5 +1,13 @@
 import { Tokenizer, Token, TokenType } from './tokenizer'
-import { Node, Located, Location, NodeType, Statement, Program } from '../nodes'
+import {
+  Node,
+  Located,
+  NodeType,
+  Statement,
+  Program,
+  Position,
+  Location,
+} from '../nodes'
 import { createError } from './create-error'
 import { TokenReader } from './token-reader'
 
@@ -8,9 +16,18 @@ import { parseStmt } from './statement/index'
 import { parseCompound } from './compound'
 import { buildDebug } from '../utils/debug'
 
-const debug = buildDebug('parser')
+const debug = {
+  start: buildDebug('parser:start'),
+  finish: buildDebug('parser:finish'),
+  trace: buildDebug('parser:trace'),
+}
 
 export const parse = (source: string): Program => new Parser(source).parse()
+
+interface ParsingState {
+  token: Token
+  pos: Position
+}
 
 export class Parser extends TokenReader {
   source: string
@@ -23,16 +40,45 @@ export class Parser extends TokenReader {
     this.source = source
   }
 
-  parse(): Program {
+  parse(): Located<Program> {
     const loc = this.startNode()
 
     const body = parseCompound<Statement>(this, parseStmt)
 
-    // TODO: overload builder func type,
-    // to detect we pass `loc` and its return type is surely `Located`
     return this.finishNode({ type: 'Program', body, loc })
   }
 
+  parseNode<T extends NodeType>(
+    token: Token,
+    parse: (state: ParsingState) => Node & { type: T }
+  ): Located<Node & { type: T }> {
+    debug.start(token)
+
+    const state: ParsingState = {
+      token,
+      pos: token
+        ? token.loc.start
+        : // Especially for parsing Program node since the beginning of file
+          // has no token
+          { offset: 0, line: 1, column: 1 },
+    }
+
+    const node = parse(state)
+
+    if (!node.loc) {
+      node.loc = {
+        start: state.pos,
+        end: this.getCurrentToken().loc.end,
+      }
+    }
+
+    debug.finish(node)
+
+    // @ts-expect-error FIXME:
+    return node
+  }
+
+  // FIXME: remove
   startNode(): Location {
     const startToken = this.getCurrentToken()
     const start = startToken
@@ -49,17 +95,13 @@ export class Parser extends TokenReader {
     }
   }
 
+  // FIXME: remove
   finishNode<T extends NodeType>(
     node: Located<Node & { type: T }>
   ): Located<Node & { type: T }> {
     node.loc.end = this.getCurrentToken().loc.end
 
-    if (debug.enabled) {
-      const log = { ...node }
-      // @ts-expect-error
-      delete log.loc
-      debug(log)
-    }
+    debug.finish(node)
 
     return node
   }
