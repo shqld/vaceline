2b0e952d1ab06f52f5fceadd2af401277cd04224
Sho Miyamoto <me@shomiyamoto.dev>
2021-02-20 16:47:13 +0900

    fix: add config files to components/utils

---
+++ components/utils/__tests__/index.ts
@@ -0,0 +1,5 @@
+describe('components', () => {
+  describe('utils', () => {
+    test.todo('')
+  })
+})
--- components/utils/src/assert.ts
+++ components/utils/src/assert.ts
@@ -1,6 +1,6 @@
 export const assert: typeof console.assert =
   // FIXME: remove undefined handling after server-side is also built by webpack
-  typeof BUILD_ENV === 'undefined' && BUILD_ENV === 'development'
+  typeof BUILD_ENV !== 'undefined' && BUILD_ENV === 'development'
     ? console.assert
     : // eslint-disable-next-line @typescript-eslint/no-empty-function
       () => {}
